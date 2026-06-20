09ab7fbaf406d5dfea8526d9c1b64e8bb160a0a3
Murasame-101 <shogoukisinji@gmail.com>
2026-06-20 16:01:29 +0900

    API取得

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+"use client"
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const[weatherData, setWeatherData]= useState<any>({});
+
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=35.689&lon=139.692&exclude=hourly,daily&appid=bb58c5a33473c48135cac6fd25000496&lang=ja&units=metric")
+  .then((response) => response.json())
+    .then((data) => {
+      setWeatherData(data);
+    });
+  const tenkou=weatherData.weather?.at(0)?.main;
+    console.log("tenkou", tenkou);
+
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -15,8 +29,10 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file. <br></br>  
-            天気アプリへようこそ
+            天気アプリへようこそrun 
           </h1>
+          <p>今日の天気：{tenkou}</p>
+
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
