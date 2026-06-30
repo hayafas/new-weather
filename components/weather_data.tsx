89405178f7ab02aab925198328cc184afacd6454
Teptep <liucr0704@gmail.com>
2026-06-30 19:11:25 +0900

    2026.6.30 ryu_mainpage_sakusei 変更点 ・現在位置（緯度経度）取得してその座標における天気を取得できるようにした（めっちゃ時間かかるのが欠点） ・背景色がダークモードとライトモードで切り替わるようにできた ・componentsの中身をAPIから取得したデータを収納した場所にした ・APIkeyが漏洩しないように.envにAPIKEYを隠し、またそれでもAPIが読み込めるようにappの中にapi\weather\route.tsを作りました（ひろきが）。 ・Blur（motion）のアニメーションを追加した ・APIから取得した天気のデータをweather_dataで表示するようにした。

---
+++ components/weather_data.tsx
@@ -0,0 +1,76 @@
+"use client"; // データをコンポーネント内で管理するために必須です
+
+import { useState, useEffect } from "react";
+import BlurText from "../components/BlurText";
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
+
+export default function Weather() {
+  const [weatherData, setWeatherData] = useState<any>({});
+  const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  useEffect(() => {
+    // ブラウザが位置情報に対応しているかチェック
+    if (!navigator.geolocation) {
+      setErrorMsg("お使いのブラウザは位置情報に対応していません。");
+      setLoading(false);
+      return;
+    }
+
+    // 現在位置（GPS/ネットワーク）を取得
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;  // 緯度を取得
+        const lon = position.coords.longitude; // 経度を取得
+        // URLに取得した lat と lon を埋め込む（ついでに単位をセルシウス、言語を日本語に設定）
+        const url = location.origin + `/api/weather?lat=${lat}&lon=${lon}`;
+        // 取得した現在位置のURLでフェッチ
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("APIレスポンス:", data);
+            setWeatherData(data);
+            console.log("https://openweathermap.org/payload/api/media/file/" + data.weather?.[0]?.icon + ".png")
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("APIクライアントエラー:", error);
+            setErrorMsg("天気データの取得に失敗しました。");
+            setLoading(false);
+          });
+      },
+
+      (error) => {
+        // ユーザーが位置情報の許可を拒否した場合などのエラーハンドリング
+        console.error("位置情報取得エラー:", error);
+        setErrorMsg("位置情報の取得が許可されませんでした。");
+        setLoading(false);
+      }
+    );
+  }, []); // 最後の [ ] が「最初の1回だけ実行してね」というお守り
+
+  return (
+    <>
+      <div className="text-gray-600 dark:text-gray-400">
+        <BlurText
+          text={weatherData.weather?.[0]?.main ? "今日の天気：" + weatherData.weather?.[0]?.main : "今日の天気：読み込み中..."}
+          delay={200}
+          stepDuration={0.65}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="weather-status"
+        />
+        <img src={weatherData.weather?.[0]?.icon
+          ? "https://openweathermap.org/payload/api/media/file/" + weatherData.weather?.[0]?.icon + ".png"
+          : "/loading_icon.png"}
+          alt={"天気アイコン"}
+          width="40px"
+        />
+      </div>
+    </>
+
+  )
+}
